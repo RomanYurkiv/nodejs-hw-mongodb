ee92ac095c87fbee95f491bda224e3f872ecf09d
Trafalgar <yurkiv.r.official@gmail.com>
2024-06-23 13:16:00 +0300

    fix

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,20 +1,51 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
+// import { env } from '../utils/env.js';
+
+// export const initMongoConnection = async () => {
+//   try {
+//     const user = env('MONGODB_USER');
+//     const pwd = env('MONGODB_PASSWORD');
+//     const url = env('MONGODB_URL');
+//     const db = env('MONGODB_DB');
+
+//     await mongoose.connect(
+//       "mongodb+srv://yurkivrofficial:zguobNrNhtvWhn61@cluster0romanyurkiv.leibckr.mongodb.net/",
+//     );
+//     console.log('Mongo connection successfully established!');
+//   } catch (e) {
+//     console.log('Error while setting up mongo connection', e);
+//     throw e;
+//   }
+// };
+
+import mongoose from 'mongoose';
 import { env } from '../utils/env.js';
 
 export const initMongoConnection = async () => {
   try {
     const user = env('MONGODB_USER');
     const pwd = env('MONGODB_PASSWORD');
-    const url = env('MONGODB_URL');
+    const url = env('MONGODB_URL'); // This should be the cluster URL without "mongodb+srv://"
     const db = env('MONGODB_DB');
 
-    await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
-    );
+    console.log(user);
+    console.log(pwd);
+    console.log(url);
+    console.log(db);
+
+    // Construct the connection string
+    const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}`  
+    console.log(connectionString);
+
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log('Mongo connection successfully established!');
   } catch (e) {
-    console.log('Error while setting up mongo connection', e);
+    console.error('Error while setting up mongo connection', e);
     throw e;
   }
 };
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { env } from './utils/env.js';
 
 import { getAllContacts, getContactById } from './services/contacts.js';
 
-const PORT = Number(env('PORT', '3000'));
+const PORT = (env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
