119624fd2572cc09c083345b1e5c5a01d9cbeea2
Trafalgar <yurkiv.r.official@gmail.com>
2024-06-27 11:02:13 +0300

    error correction

--- src/db/contact.js
+++ src/db/contact.js
@@ -1,38 +1,35 @@
 import { model, Schema } from 'mongoose';
 
-const Contact = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
-  },
+const Contact = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
 
-  phoneNumber: {
-    type: String,
-    required: [true, 'Set phone number for contact'],
-  },
+    phoneNumber: {
+      type: String,
+      required: [true, 'Set phone number for contact'],
+    },
 
-  email: {
-    type: String,
-    optional: true,
-  },
+    email: {
+      type: String,
+      optional: true,
+    },
 
-  isFavorite: {
-    type: Boolean,
-    default: false,
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'personal', 'home'],
+      default: 'personal',
+    },
   },
-  contactType: {
-    type: String,
-    enum: ['work', 'personal', 'home'],
-    default: 'personal',
+  {
+    timestamps: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+);
 
 export const ContactsCollection = model('contacts', Contact);
