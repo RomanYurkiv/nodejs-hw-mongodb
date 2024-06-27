cbe04a909c9d264360b0fd17dd1c9c11b4c42860
Trafalgar <yurkiv.r.official@gmail.com>
2024-06-27 10:51:13 +0300

    update

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,23 +1,3 @@
-// import mongoose from 'mongoose';
-
-// import { env } from '../utils/env.js';
-
-// export const initMongoConnection = async () => {
-//   try {
-//     const user = env('MONGODB_USER');
-//     const pwd = env('MONGODB_PASSWORD');
-//     const url = env('MONGODB_URL');
-//     const db = env('MONGODB_DB');
-
-//     await mongoose.connect(
-//       "mongodb+srv://yurkivrofficial:zguobNrNhtvWhn61@cluster0romanyurkiv.leibckr.mongodb.net/",
-//     );
-//     console.log('Mongo connection successfully established!');
-//   } catch (e) {
-//     console.log('Error while setting up mongo connection', e);
-//     throw e;
-//   }
-// };
 
 import mongoose from 'mongoose';
 import { env } from '../utils/env.js';
@@ -26,7 +6,7 @@ export const initMongoConnection = async () => {
   try {
     const user = env('MONGODB_USER');
     const pwd = env('MONGODB_PASSWORD');
-    const url = env('MONGODB_URL'); // This should be the cluster URL without "mongodb+srv://"
+    const url = env('MONGODB_URL');
     const db = env('MONGODB_DB');
 
     console.log(user);
@@ -34,7 +14,6 @@ export const initMongoConnection = async () => {
     console.log(url);
     console.log(db);
 
-    // Construct the connection string
     const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}`  
     console.log(connectionString);
 
