79d63b0b5e3195076d42af061db695691f134871
Trafalgar <yurkiv.r.official@gmail.com>
2024-08-16 12:34:12 +0300

    update

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -42,23 +42,6 @@ const setAuthContactId = (req) => {
   return authContactId;
 };
 
-// export const getContactByIdController = async (req, res, next) => {
-//   const { contactId } = req.params;
-//   try {
-//     const contact = await getContactById(contactId);
-//     if (!contact) {
-//       return next(createHttpError(404, `Contact with id ${contactId} not found`));
-//     }
-//     res.json({
-//       status: 200,
-//       message: `Successfully found contact with id ${contactId}!`,
-//       data: contact,
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
 export const getContactByIdController = async (req, res, next) => {
   const authContactId = setAuthContactId(req);
   const contact = await getContactById(authContactId);
@@ -68,19 +51,10 @@ export const getContactByIdController = async (req, res, next) => {
   }
   res.status(200).json({
     status: 200,
-    message: `Successfully found contact with id ${authContactId}!`,
+    message: `Successfully found contact with id ${req.params.contactId}!`, // Використання чистого contactId
     data: contact,
   });
 };
-
-// export const createContactController = async (req, res, next) => {
-//   const contact = await createContact(req.body);
-//   res.status(201).json({
-//     status: 201,
-//     message: 'Successfully created contact!',
-//     data: contact,
-//   });
-// };
 
 export const createContactController = async (req, res) => {
   const contact = await createContact({ userId: req.user._id, ...req.body });
@@ -91,16 +65,6 @@ export const createContactController = async (req, res) => {
   });
 };
 
-// export const deleteContactController = async (req, res, next) => {
-//   const { contactId } = req.params;
-//   const contact = await deleteContact(contactId);
-
-//   if (!contact) {
-//     return next(createHttpError(404, `Contact with id ${contactId} not found`));
-//   }
-
-//   res.status(204).send();
-// };
 
 export const deleteContactController = async (req, res, next) => {
   const authContactId = setAuthContactId(req);
@@ -113,22 +77,6 @@ export const deleteContactController = async (req, res, next) => {
   res.status(204).send();
 };
 
-// export const upsertContactController = async (req, res, next) => {
-//   const { contactId } = req.params;
-//   const result = await updateContact(contactId, req.body, { upsert: true });
-
-//   if (!result) {
-//     return next(createHttpError(404, `Contact with id ${contactId} not found`));
-//   }
-
-//   const status = result.isNew ? 201 : 200;
-
-//   res.status(status).json({
-//     status,
-//     message: `Successfully upserted contact with id ${contactId}!`,
-//     data: result.contact.value,
-//   });
-// };
 
 export const upsertContactController = async (req, res, next) => {
   const authContactId = setAuthContactId(req);
@@ -147,25 +95,6 @@ export const upsertContactController = async (req, res, next) => {
   });
 };
 
-// export const patchContactController = async (req, res, next) => {
-//   const { contactId } = req.params;
-//   try {
-//     const result = await updateContact(contactId, req.body);
-
-//     if (!result) {
-//       return next(createHttpError(404, `Contact with id ${contactId} not found`));
-//     }
-
-//     res.json({
-//       status: 200,
-//       message: `Successfully patched contact with id ${contactId}!`,
-//       data: result.contact.value,
-//     });
-//   } catch (error) {
-//     console.error('Error in patchContactController:', error);
-//     next(error);
-//   }
-// };
 
 export const patchContactController = async (req, res, next) => {
   const authContactId = setAuthContactId(req);
