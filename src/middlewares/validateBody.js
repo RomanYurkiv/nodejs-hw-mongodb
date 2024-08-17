ad69f62d6fcfc5f3620ad61a56fc9552dccc2b08
Trafalgar <yurkiv.r.official@gmail.com>
2024-08-17 12:35:17 +0300

    fix error massege

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,15 +1,14 @@
 import createHttpError from 'http-errors';
 
-export const validateBody = (schema) => async (req, res, next) => {
-  try {
-    await schema.validateAsync(req.body, {
-      abortEarly: false,
-    });
-    next();
-  } catch (err) {
-    const error = createHttpError(400, 'Bad Request', {
-      errors: err.details.map((detail) => detail.message),
-    });
-    next(error);
-  }
+export const validateBody = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    
+    if (error) {
+      const errorDetails = error.details.map(detail => detail.message).join(', ');
+      next(createHttpError(400, `Validation error: ${errorDetails}`));
+    } else {
+      next();
+    }
+  };
 };
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,19 +3,45 @@ import Joi from 'joi';
 const namePattern = /^[A-Z][a-zA-Z]* [A-Z][a-zA-Z]*$/;
 
 export const createContactsSchema = Joi.object({
-  name: Joi.string().pattern(namePattern).required(),
-  email: Joi.string().email().required(),
-  phoneNumber: Joi.string().pattern(/^\+\d{12}$/).required(),
-  isFavourite: Joi.boolean().required(),
-  contactType: Joi.string().valid('personal', 'work').required(),
-  parentId: Joi.string().required(),
+  name: Joi.string().pattern(namePattern).required().messages({
+    'string.pattern.base': `"name" must start with a capital letter and contain a space between first and last name`,
+    'any.required': `"name" is a required field`,
+  }),
+  email: Joi.string().email().required().messages({
+    'string.email': `"email" must be a valid email address`,
+    'any.required': `"email" is a required field`,
+  }),
+  phoneNumber: Joi.string().pattern(/^\+\d{12}$/).required().messages({
+    'string.pattern.base': `"phoneNumber" must be in the format +XXXXXXXXXXXX (12 digits)`,
+    'any.required': `"phoneNumber" is a required field`,
+  }),
+  isFavourite: Joi.boolean().required().messages({
+    'any.required': `"isFavourite" is a required field`,
+  }),
+  contactType: Joi.string().valid('personal', 'work').required().messages({
+    'any.only': `"contactType" must be either 'personal' or 'work'`,
+    'any.required': `"contactType" is a required field`,
+  }),
+  parentId: Joi.string().required().messages({
+    'any.required': `"parentId" is a required field`,
+  }),
 });
 
 export const updateContactsSchema = Joi.object({
-  name: Joi.string().pattern(namePattern),
-  email: Joi.string().email(),
-  phoneNumber: Joi.string().pattern(/^\+\d{12}$/),
+  name: Joi.string().pattern(namePattern).messages({
+    'string.pattern.base': `"name" must start with a capital letter and contain a space between first and last name`,
+  }),
+  email: Joi.string().email().messages({
+    'string.email': `"email" must be a valid email address`,
+  }),
+  phoneNumber: Joi.string().pattern(/^\+\d{12}$/).messages({
+    'string.pattern.base': `"phoneNumber" must be in the format +XXXXXXXXXXXX (12 digits)`,
+  }),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('personal', 'work'),
-  parentId: Joi.string().required(),
+  contactType: Joi.string().valid('personal', 'work').messages({
+    'any.only': `"contactType" must be either 'personal' or 'work'`,
+  }),
+  parentId: Joi.string().required().messages({
+    'any.required': `"parentId" is a required field`,
+  }),
 }).min(1);
