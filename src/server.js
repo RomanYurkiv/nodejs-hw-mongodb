b9644e698c942a5400c1b094ff32162d95eaf951
Trafalgar <yurkiv.r.official@gmail.com>
2024-08-27 22:36:18 +0300

    intermediate

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+
 import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
 import { env } from './utils/env.js';
+
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -16,6 +21,8 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(
     pino({
@@ -24,10 +31,6 @@ export const setupServer = () => {
       },
     }),
   );
-
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
 
   app.use(router);
 
@@ -38,7 +41,4 @@ export const setupServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  
 };
